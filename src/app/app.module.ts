9cd7288d161df01faa5b145b44c67a2076c2df31
simonpla <63154196+simonpla@users.noreply.github.com>
2023-01-29 17:35:06 +0100

    basic website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TrianglesComponent } from './triangles/triangles.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrianglesComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule
